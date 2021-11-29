b1cb958d71889b5ae83ee7b1b5dc94e18398d5de
WanSnow <641235667@qq.com>
2021-11-29 23:17:02 +0800

    add nsq docker

--- script/make_nsq.sh
+++ script/make_nsq.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-HOST=127.0.0.1
-PORT=9697
-
-docker run --name lookupd -p 4160:4160 -p 4161:4161 nsqio/nsq /nsqlookupd
-docker run --name nsqd -p 4150:4150 -p 4151:4151 \
-    nsqio/nsq /nsqd \
-    --broadcast-address=${HOST} \
-    --lookupd-tcp-address=${HOST}:${PORT}
+cd docker/nsq \
+  && docker-compose up -d
